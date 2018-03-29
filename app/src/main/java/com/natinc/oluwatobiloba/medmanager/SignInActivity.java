1fc130f2f4881633ef535ba9267231da85717b14
natintosh <ogunye4nao@gmail.com>
2018-03-29 20:50:58 +0100

    second commit

---
+++ app/src/main/java/com/natinc/oluwatobiloba/medmanager/SignInActivity.java
@@ -0,0 +1,13 @@
+package com.natinc.oluwatobiloba.medmanager;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
