b1aa7c106b051f36a270ef0544ec7f1f8fb536f7
natintosh <ogunye4nao@gmail.com>
2018-03-30 22:17:36 +0100

    seventh commit: Minor Changes made

--- app/src/main/java/com/natinc/oluwatobiloba/medmanager/EditProfileActivity.java
+++ app/src/main/java/com/natinc/oluwatobiloba/medmanager/EditProfileActivity.java
@@ -2,12 +2,27 @@ package com.natinc.oluwatobiloba.medmanager;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import lib.kingja.switchbutton.SwitchMultiButton;
 
 public class EditProfileActivity extends AppCompatActivity {
+
+    String mGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        SwitchMultiButton mSwitchMultiButton = findViewById(R.id.switchmultibutton);
+        mSwitchMultiButton.setOnSwitchListener(new SwitchMultiButton.OnSwitchListener() {
+            @Override
+            public void onSwitch(int position, String tabText) {
+                Toast.makeText(EditProfileActivity.this, tabText, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
