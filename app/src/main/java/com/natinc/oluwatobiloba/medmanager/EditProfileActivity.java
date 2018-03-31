7646d21c511a9540688d89b5a88cba4b18bd6198
natintosh <ogunye4nao@gmail.com>
2018-03-31 15:56:24 +0100

    eight commit: Minor Changes made

--- app/src/main/java/com/natinc/oluwatobiloba/medmanager/EditProfileActivity.java
+++ app/src/main/java/com/natinc/oluwatobiloba/medmanager/EditProfileActivity.java
@@ -1,7 +1,9 @@
 package com.natinc.oluwatobiloba.medmanager;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import lib.kingja.switchbutton.SwitchMultiButton;
@@ -9,13 +11,16 @@ import lib.kingja.switchbutton.SwitchMultiButton;
 public class EditProfileActivity extends AppCompatActivity {
 
     String mGender;
+    SwitchMultiButton mSwitchMultiButton;
+    TextView mEditNameTextView, mEditEmailTextView;
+    TextInputLayout mPhoneNumberTextInputLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
 
-        SwitchMultiButton mSwitchMultiButton = findViewById(R.id.switchmultibutton);
+        mSwitchMultiButton = findViewById(R.id.switchmultibutton);
         mSwitchMultiButton.setOnSwitchListener(new SwitchMultiButton.OnSwitchListener() {
             @Override
             public void onSwitch(int position, String tabText) {
