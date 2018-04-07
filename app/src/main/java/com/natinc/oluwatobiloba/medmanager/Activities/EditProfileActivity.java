d84bc33608e7585f47023f822c254e07e80e086d
natintosh <ogunye4nao@gmail.com>
2018-04-07 15:13:38 +0100

    Twelveth Commit: Reorganising The project

--- app/src/main/java/com/natinc/oluwatobiloba/medmanager/Activities/EditProfileActivity.java
+++ app/src/main/java/com/natinc/oluwatobiloba/medmanager/Activities/EditProfileActivity.java
@@ -30,8 +30,8 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.natinc.oluwatobiloba.medmanager.Models.User;
 import com.natinc.oluwatobiloba.medmanager.R;
-import com.natinc.oluwatobiloba.medmanager.User;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
