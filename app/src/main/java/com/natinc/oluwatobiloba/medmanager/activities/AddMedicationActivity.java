00454ba06cca40800a0015d1e86781b1a5c0fa06
natintosh <ogunye4nao@gmail.com>
2018-04-08 23:31:31 +0100

    Thirteen Commit

--- app/src/main/java/com/natinc/oluwatobiloba/medmanager/activities/AddMedicationActivity.java
+++ app/src/main/java/com/natinc/oluwatobiloba/medmanager/activities/AddMedicationActivity.java
@@ -1,13 +1,23 @@
 package com.natinc.oluwatobiloba.medmanager.activities;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
 import com.natinc.oluwatobiloba.medmanager.R;
+import com.natinc.oluwatobiloba.medmanager.ui.DatePickerFragment;
 
-public class AddMedicationActivity extends AppCompatActivity {
+public class AddMedicationActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
     TextInputLayout mNameLayout, mDescriptionLayout,
             mPillsLayout, mDoseLayout, mIntervalLayout, mStartLayout, mEndLayout;
@@ -21,6 +31,19 @@ public class AddMedicationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_medication);
 
         initializingVariables();
+        invalidateInputs();
+
+        mStartEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(), "datepicker");
+            }
+        });
+    }
+
+    private void invalidateInputs() {
+
     }
 
     private void initializingVariables() {
@@ -42,5 +65,31 @@ public class AddMedicationActivity extends AppCompatActivity {
 
     }
 
-    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.add_options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menu_add_medication) {
+            // Validate inputs and add medications to firebase
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+        DialogFragment timePicker = new DatePickerFragment();
+        timePicker.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    @Override
+    public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+
+    }
 }
