6b57bb8913c4df8451a58d8047212aa9df8b3099
natintosh <ogunye4nao@gmail.com>
2018-04-08 14:12:30 +0100

    Twelveth Commit: Reorganising The project

--- app/src/main/java/com/natinc/oluwatobiloba/medmanager/activities/DashBoardActivity.java
+++ app/src/main/java/com/natinc/oluwatobiloba/medmanager/activities/DashBoardActivity.java
@@ -3,26 +3,28 @@ package com.natinc.oluwatobiloba.medmanager.activities;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.natinc.oluwatobiloba.medmanager.R;
 import com.natinc.oluwatobiloba.medmanager.ui.UserDetailsFragment;
-import com.natinc.oluwatobiloba.medmanager.utils.FirebaseUtils;
 
 public class DashBoardActivity extends AppCompatActivity {
 
+
+    FirebaseAuth mFirebaseAuth;
+    FirebaseUser mFirebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
 
-        FirebaseUser firebaseUser = FirebaseUtils.getFirebaseUser();
-
-        Log.v("USer Name", firebaseUser.getDisplayName());
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
 
         UserDetailsFragment userDetailsFragment = new UserDetailsFragment();
+        userDetailsFragment.setFirebaseUser();
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
                 .add(R.id.user_details_container, userDetailsFragment)
