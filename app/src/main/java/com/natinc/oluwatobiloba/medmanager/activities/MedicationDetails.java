d4a6222db504acd9a7e825d6e5a68ca510c34ab7
natintosh <ogunye4nao@gmail.com>
2018-04-10 20:28:32 +0100

    Nineteenth Commit

---
+++ app/src/main/java/com/natinc/oluwatobiloba/medmanager/activities/MedicationDetails.java
@@ -0,0 +1,15 @@
+package com.natinc.oluwatobiloba.medmanager.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.natinc.oluwatobiloba.medmanager.R;
+
+public class MedicationDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medication_details);
+    }
+}
